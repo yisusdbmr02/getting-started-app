35a92a5920b7a787abe004a904b80c6d8198d557
yisusdbmr02 <jesusredrojo@gmail.com>
2024-01-30 09:36:39 +0100

    x

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">HEllo World</p>
+                <p className="text-center">Holaaa</p>
             )}
             {items.map(item => (
                 <ItemDisplay
